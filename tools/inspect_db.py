42a23f455848d73f4b08f0051d046492b495a2ca
Felix Schmidt <f.schmidt@fz-juelich.de>
2025-09-17 11:30:00 +0200

    Small update to inspect_db.

--- tools/inspect_db.py
+++ tools/inspect_db.py
@@ -38,7 +38,7 @@ if __name__ == '__main__':
     # Config
 
     max_rows_to_print = 35
-    scores = ['dwn_train_acc', 'dwn_train_acc_std'] #['dwn_train_acc', 'dwn_train_acc_std']
+    scores = ['dwn_dev_acc_last', 'dwn_dev_acc_last_std', 'dwn_test_acc_last', 'dwn_test_acc_last_std'] #['dwn_train_acc', 'dwn_train_acc_std']
 
     # Script
 
